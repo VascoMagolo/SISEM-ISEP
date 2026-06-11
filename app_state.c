0488a51771d3f0e6cc704d7191e4ad3e5f67b701
Diogo Nogueira <isdiogon@gmail.com>
2026-06-12 00:07:33 +0100

    extract shared volatile state into app_state module

---
+++ app_state.c
@@ -0,0 +1,6 @@
+#include "app_state.h"
+
+volatile uint16_t potentiometer_value = 0;
+volatile bool     led_blinking        = false;
+volatile uint32_t led_tick_interval   = 100; // 100 ticks * 10ms = 1000ms default
+volatile uint32_t timer_interval      = 2000; // full blink period in ms
---
+++ app_state.h
@@ -0,0 +1,12 @@
+#ifndef APP_STATE_H_
+#define APP_STATE_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+extern volatile uint16_t potentiometer_value;
+extern volatile bool     led_blinking;
+extern volatile uint32_t led_tick_interval;
+extern volatile uint32_t timer_interval;
+
+#endif /* APP_STATE_H_ */
